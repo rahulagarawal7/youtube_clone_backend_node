53e5244f462ed3970bbed4241466bb34b2cce4ac
rahulagarawal7 <156163576+rahulagarawal7@users.noreply.github.com>
2025-09-27 11:11:36 +0530

    aaded cors

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ import commentRoutes from "./routes/commentRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://youtube-clone-taupe-two.vercel.app/"], 
+  credentials: true
+}));
+
 app.use(express.json());
 
 // DB connect
